27d63eb5e681e51cf85d779855606b527ceae7f9
Gabriel Fonseca <gabrielfonsecaoficial@gmail.com>
2021-07-06 16:11:48 -0300

    [implementação] - criação do componente Feed

--- nextjs-clone-tiktok/src/components/User/User.js
+++ nextjs-clone-tiktok/src/components/User/User.js
@@ -1,7 +1,15 @@
 import React from 'react'
 
+import { Container, Avatar, Info } from './s_user'
+
 export default function User ({ user }) {
   return (
-    <span>{user.name}</span>
+    <Container>
+      <Avatar src={user.avatar} />
+      <Info>
+        <a> {user.name} </a>
+        <span> {user.username} </span>
+      </Info>
+    </Container>
   )
 }
